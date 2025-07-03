00991dd3f12c708b201cb10bc4cd23d26839acd5
theahmed619 <theahmed619@gmail.com>
2025-07-03 18:11:42 +0530

    remove faaltu projects

--- TodoReactApp/TodoFrontend/src/service/todoService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = "http://localhost:8090"; // Change if needed
-
-export const getAllTodos = () => axios.get(`${BASE_URL}/showall`);
-export const addTodo = (todo) => axios.post(`${BASE_URL}/add`, todo);
-export const toggleTodo = (id) => axios.put(`${BASE_URL}/toggle/${id}`);
-export const deleteTodo = (id) => axios.delete(`${BASE_URL}/delete/${id}`);
